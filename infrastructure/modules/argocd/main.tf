36095c1cf0f7efdac16ca8c53f57dc3c1d1870f4
Trần Giang <hoanggiangtran239@gmail.com>
2026-05-11 17:27:25 +0700

    fix root app

--- infrastructure/modules/argocd/main.tf
+++ infrastructure/modules/argocd/main.tf
@@ -7,19 +7,44 @@ resource "kubernetes_namespace_v1" "argocd" {
 resource "helm_release" "argocd" {
   name       = "argocd"
   namespace  = "argocd"
+
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "5.51.6"
 
   create_namespace = true
 
-  values = [file("${path.module}/values.yaml")]
+  values = [
+    file("${path.module}/values.yaml")
+  ]
 
-  depends_on = [kubernetes_namespace_v1.argocd]
+  depends_on = [
+    kubernetes_namespace_v1.argocd
+  ]
 
   timeout         = 1200
   wait            = true
   force_update    = true
   recreate_pods   = true
   cleanup_on_fail = true
+}
+
+# Wait ArgoCD server ready
+resource "time_sleep" "wait_argocd" {
+  depends_on = [
+    helm_release.argocd
+  ]
+
+  create_duration = "120s"
+}
+
+# Bootstrap App Of Apps
+resource "kubernetes_manifest" "root_app" {
+  manifest = yamldecode(
+    file("${path.root}/../../../k8s-gitops/bootstrap/app-of-apps.yaml")
+  )
+
+  depends_on = [
+    time_sleep.wait_argocd
+  ]
 }
