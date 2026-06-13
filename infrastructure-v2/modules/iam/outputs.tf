63409dff8d21387a89ef4d2c79169b3fae012eed
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-06-13 21:14:02 +0700

    Update outputs.tf

--- infrastructure-v2/modules/iam/outputs.tf
+++ infrastructure-v2/modules/iam/outputs.tf
@@ -8,6 +8,14 @@ output "eks_node_role_arn" {
   value       = aws_iam_role.eks_node_role.arn
 }
 
+######################## addd-newwwwwwwwwwwww
+output "eks_node_role_name" {
+  value       = aws_iam_role.node.name # Thay "node" bằng tên resource aws_iam_role tạo node group role của bạn
+  description = "Tên của IAM Role dành cho Node Group"
+}
+########################
+
+
 # output "alb_controller_role_arn" {
 #   description = "ALB controller role ARN"
 #   value       = aws_iam_role.alb_controller_role.arn
