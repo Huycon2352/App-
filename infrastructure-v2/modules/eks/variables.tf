a7f6b97f18f19a430aeb624d260dac274ac0fb07
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-06-13 21:11:24 +0700

    Update variables.tf

--- infrastructure-v2/modules/eks/variables.tf
+++ infrastructure-v2/modules/eks/variables.tf
@@ -23,3 +23,16 @@ variable "cluster_security_group_id" {
   description = "Security group ID for EKS cluster"
   type        = string
 }
+
+###################addd -on newwwwwwwwwwwww
+
+variable "ebs_csi_addon_version" {
+  type        = string
+  default     = "v1.51.0-eksbuild.1"
+  description = "Phiên bản EBS CSI Driver"
+}
+
+variable "node_role_name" {
+  type        = string
+  description = "Tên (Name) của IAM Role đang dùng cho Node Group"
+}
